dd5b5b067c360d0ea05ca502a908d8ed2f5851c8
Narmina Aghayeva <nargayeva@gmail.com>
2021-03-03 14:51:16 +0400

    map added

--- assets/js/app.js
+++ assets/js/app.js
@@ -63,3 +63,10 @@ $(document).ready(function() {
     });
 });
 
+function myMap() {
+    var mapProp= {
+      center:new google.maps.LatLng(51.508742,-0.120850),
+      zoom:5,
+    };
+    var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
+    }

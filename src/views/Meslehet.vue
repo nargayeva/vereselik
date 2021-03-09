e5700232aada0ec16e89481a58d454db991c7796
fuadaghazada <fuad.aghazada98@gmail.com>
2021-03-09 22:55:00 +0400

    Adding validation for interactive for

--- src/views/Meslehet.vue
+++ src/views/Meslehet.vue
@@ -30,24 +30,33 @@
                     <i class="fas fa-map contact"></i>
                     <span class="contact">Bakı şəhəri, AZ1073, İnşaatçılar prospekti 1</span>
                 </div>
-
             </div>
-
 
             <div class="col-md-6 col-12">
                 <span class="contact-header">Müraciət forması:</span>
-                <form class="mt-3">
+                <form class="mt-3" @submit.prevent="onSubmit">
                     <div class="form-row">
                         <div class="form-group col-md-6">
-                            <input type="text" class="form-control needsvalidation" id="fullname"
-                                   placeholder="Ad və soyad">
-                            <div class="invalid-feedback">
+                            <input
+                                    type="text"
+                                    class="form-control needsvalidation"
+                                    id="fullname"
+                                    placeholder="Ad və soyad"
+                                    v-model="name"
+                            >
+                            <div v-if="!nameValidated" class="error">
                                 Zəhmət olmasa, adınızı və soyadınızı düzgün daxil edin.
                             </div>
                         </div>
                         <div class="form-group col-md-6">
-                            <input type="text" class="form-control needsvalidation" id="fin" placeholder="FİN">
-                            <div class="invalid-feedback">
+                            <input
+                                    type="text"
+                                    class="form-control needsvalidation"
+                                    id="fin"
+                                    placeholder="FİN"
+                                    v-model="fin"
+                            >
+                            <div v-if="!finValidated" class="error">
                                 Zəhmət olmasa, düzgün FIN daxil edin.
                             </div>
                         </div>
@@ -55,30 +64,26 @@
 
                     <div class="form-row">
                         <div class="form-group col-md-6">
-                            <input type="email" class="form-control needsvalidation" id="email" placeholder="E-poçt">
-                            <div class="invalid-feedback">
+                            <input type="email" class="form-control needsvalidation" id="email" placeholder="E-poçt" v-model="email">
+                            <div v-if="!emailValidated" class="error">
                                 Zəhmət olmasa, düzgün email daxil edin.
                             </div>
-                            <div class="valid-feedback">
-                                Doğrudur.
-                            </div>
+                            <div class="valid-feedback" />
                         </div>
                         <div class="form-group col-md-6">
-                            <input type="tel" class="form-control needsvalidation" id="phone" placeholder="Telefon">
-                            <div class="invalid-feedback">
+                            <input type="tel" class="form-control needsvalidation" id="phone" placeholder="Telefon" v-model="phone">
+                            <div v-if="!phoneValidated" class="error">
                                 Zəhmət olmasa, düzgün telefon nömrəsi daxil edin.
                             </div>
-                            <div id="valid-feedback">
-
-                            </div>
+                            <div id="valid-feedback" />
                         </div>
                     </div>
 
                     <div class="input-group mb-3">
-                        <textarea class="form-control mw-5" aria-label="With textarea" placeholder="Sual"></textarea>
+                        <textarea class="form-control mw-5" aria-label="With textarea" placeholder="Sual" v-model="question" />
                     </div>
 
-                    <span class="flex-button-send" id="send-button">GÖNDƏR</span>
+                    <button class="flex-button-send border-0" id="send-button">GÖNDƏR</button>
                 </form>
             </div>
         </div>
@@ -91,10 +96,75 @@
     import '../assets/css/meslehet.css';
 
     export default {
-        name: "Meslehet"
+        name: "Meslehet",
+        data() {
+            return {
+                name: null,
+                fin: null,
+                email: null,
+                phone: null,
+                question: null
+            }
+        },
+        computed: {
+            nameValidated() {
+                if (this.name === null) {
+                    return true;
+                }
+
+                const name = this.name.trim();
+                const rawParts = name.split(/[ ,]+/);
+                const validatedParts = rawParts.filter(p => p.trim());
+
+                return validatedParts.length >= 2;
+            },
+            finValidated() {
+                if (this.fin === null) {
+                    return true;
+                }
+
+                const fin = this.fin.trim();
+                return !(fin.length !== 7 || !fin.match(/^[A-Za-z0-9]+$/));
+            },
+            emailValidated() {
+                if (this.email === null) {
+                    return true;
+                }
+
+                const email = this.email.trim();
+                return email.match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
+            },
+            phoneValidated() {
+                if (this.phone === null) {
+                    return true;
+                }
+
+                const phone = this.phone.trim();
+                return phone.match(/^\+(994)(50|51|55|70|77|71|99)\d{7}$/);
+            }
+        },
+        methods: {
+            onSubmit() {
+                let newline = '%0D%0A';
+
+                const to = 'someone@yoursite.com';
+                const subject = 'Salam';
+                const body = `${this.question}${newline}${newline}${newline}Ad Soyad: ${this.name}${newline}Fin: ${this.fin}${newline}Email: ${this.email}${newline}Phone number: ${this.phone}`;
+
+                if (this.name && this.fin && this.email && this.phone && this.question &&
+                    this.nameValidated && this.finValidated && this.phoneValidated && this.emailValidated) {
+                    window.location.href = `mailto:${to}?subject=${subject}&body=${body}`;
+                }
+            }
+        }
     }
 </script>
 
 <style scoped>
-
+    .error {
+        width: 100%;
+        margin-top: 0.25rem;
+        font-size: 80%;
+        color: #dc3545;
+    }
 </style>

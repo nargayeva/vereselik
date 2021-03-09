c045b756c755cf41ef7f7e6f6b02379d2aeefd48
fuadaghazada <fuad.aghazada98@gmail.com>
2021-03-09 23:27:25 +0400

    Adding notarius detail

--- src/views/Xerite.vue
+++ src/views/Xerite.vue
@@ -16,59 +16,41 @@
     <div class="container mt-5">
         <div class="row d-flex justify-content-between">
             <div class="col-md-4 col-12 d-flex flex-column mb-3">
-              <tree-select v-if="notarius_data"
-                           v-model="selectedRegion"
-                           option-key="name"
-                           :search-keys="['name', 'region']"
-                           value-key="region"
-                           value-format="object"
-                           :options="[...notarius_data].filter((v,i,a)=>a.findIndex(t=>(t.region === v.region))===i)
+                <tree-select v-if="notarius_data"
+                             v-model="selectedRegion"
+                             option-key="name"
+                             :search-keys="['name', 'region']"
+                             value-key="region"
+                             value-format="object"
+                             :options="[...notarius_data].filter((v,i,a) => a.findIndex(t => (t.region === v.region)) === i)
 "
-              />
+                />
 
-              <tree-select v-if="selectedRegion"
-                           v-model="selectedNotariat"
-                           option-key="name"
-                           :search-keys="['name', 'region']"
-                           value-key="name"
-                           value-format="object"
-                           :options="[...notarius_data].filter(d => d.region === selectedRegion.region)"
-              />
+                <tree-select v-if="selectedRegion"
+                             v-model="selectedNotariat"
+                             option-key="name"
+                             :search-keys="['name', 'region']"
+                             value-key="name"
+                             value-format="object"
+                             :options="[...notarius_data].filter(d => d.region === selectedRegion.region)"
+                />
 
-
-<!--                &lt;!&ndash; Regions &ndash;&gt;-->
-<!--                <select v-model="selectedRegion">-->
-<!--                    <optgroup label="Bakı">-->
-<!--                        <option-->
-<!--                                v-for="(datum, i) in [...notarius_data].filter(d => d.region)"-->
-<!--                                :value="datum"-->
-<!--                                :key="i"-->
-<!--                        >-->
-<!--                            {{datum.region}}-->
-<!--                        </option>-->
-<!--                    </optgroup>-->
-<!--                </select>-->
-
-<!--                &lt;!&ndash; Notariats &ndash;&gt;-->
-<!--                <select-->
-<!--                        v-if="notariats.length"-->
-<!--                        class="js-example-basic-multiple"-->
-<!--                        v-model="selectedNotariat"-->
-<!--                >-->
-<!--                    <option-->
-<!--                            v-for="(datum, i) in notariats"-->
-<!--                            :value="datum"-->
-<!--                            :key="i"-->
-<!--                    >-->
-<!--                        {{datum.name}}-->
-<!--                    </option>-->
-<!--                </select>-->
+                <div v-if="selectedNotariat">
+                    <span>{{selectedNotariat.name}}</span>
+                    <div v-if="selectedNotariat.info">
+                        <p>{{selectedNotariat.info['person_name']}}</p>
+                        <p>{{selectedNotariat.info['email']}}</p>
+                        <p>{{selectedNotariat.info['phone']}}</p>
+                        <p>{{selectedNotariat.info['telephone']}}</p>
+                        <p>{{selectedNotariat.info['address']}}</p>
+                    </div>
+                </div>
 
             </div>
             <div class="col-md-8 col-12 map-container mb-5 googleMap">
                 <g-map
-                    :center="center"
-                    :markers="notarius_data"
+                        :center="center"
+                        :markers="notarius_data"
                 />
             </div>
         </div>
@@ -86,7 +68,7 @@
     export default {
         name: "Xerite",
         components: {
-          TreeSelect,
+            TreeSelect,
             GMap
         },
         data() {
@@ -102,11 +84,6 @@
         computed: {
             notarius_data() {
                 return json;
-            },
-            notariats() {
-                return this.selectedRegion ?
-                    [...this.notarius_data].filter(d => d.region === this.selectedRegion.region) :
-                    [];
             }
         },
         methods: {
@@ -119,6 +96,14 @@
                 handler(notariat) {
                     if (notariat) {
                         this.updateCenter(notariat.loc);
+                    }
+                },
+                deep: true
+            },
+            selectedRegion: {
+                handler(region) {
+                    if (!region) {
+                        this.selectedNotariat = null;
                     }
                 },
                 deep: true
